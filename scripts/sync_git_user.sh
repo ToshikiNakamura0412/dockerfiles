5b01a6e93a7ff43435d26d193803896fc1106a2d
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-08-31 14:13:35 +0900

    Update format

--- scripts/sync_git_user.sh
+++ scripts/sync_git_user.sh
@@ -12,27 +12,11 @@ SEARCH_STRINGS_SSH=(
 )
 TARGET_STRING_SSH="\      - type: bind\n        source: ~/.ssh\n        target: /home/user/.ssh"
 
-if [[ $1 == "disable" ]]; then
-    find ${SCRIPT_DIR}/../ -type f -name "Dockerfile" -exec sed -i "/${SEARCH_STRING_GIT}/d" {} \;
-
+function enable_git_sync() {
     for distro in ${DISTROS[@]}; do
         if [[ ${distro} != "scripts" ]]; then
-            target_file=${SCRIPT_DIR}/../${distro}/docker-compose.yml
-            count=$(grep -c ${SEARCH_STRINGS_SSH[0]} ${target_file})
-            if [[ ${count} -ne 0 ]]; then
-                target_line=$(grep -n "${SEARCH_STRINGS_SSH[0]}" ${target_file} | cut -d ":" -f 1 | head -n 1)
-                sed -i "$((target_line - 2)),$((target_line))d" ${target_file}
-            fi
-        fi
-    done
-
-    echo ""
-    echo "Disabled git sync"
-else
-    for distro in ${DISTROS[@]}; do
-        if [[ ${distro} != "scripts" ]]; then
-            target_file=${SCRIPT_DIR}/../${distro}/Dockerfile
-            count=$(grep -c ${SEARCH_STRING_GIT} ${target_file})
+            local target_file=${SCRIPT_DIR}/../${distro}/Dockerfile
+            local count=$(grep -c ${SEARCH_STRING_GIT} ${target_file})
             if [[ ${count} -eq 0 ]]; then
                 echo ${TARGET_STRING_GIT} | sed 's/\\//g' >> ${target_file}
             fi
@@ -40,7 +24,7 @@ else
             target_file=${SCRIPT_DIR}/../${distro}/docker-compose.yml
             count=$(grep -c ${SEARCH_STRINGS_SSH[0]} ${target_file})
             if [[ ${count} -eq 0 ]]; then
-                target_line=$(grep -n "${SEARCH_STRINGS_SSH[1]}" ${target_file} | cut -d ":" -f 1 | head -n 1)
+                local target_line=$(grep -n "${SEARCH_STRINGS_SSH[1]}" ${target_file} | cut -d ":" -f 1 | head -n 1)
                 sed -i "${target_line}a ${TARGET_STRING_SSH}" ${target_file}
             fi
         fi
@@ -50,4 +34,32 @@ else
     echo "Enabled git sync"
     echo ""
     echo -e "\e[33m(If you want to disable git sync, please run '$0 disable')\e[m"
-fi
+}
+
+function disable_git_sync() {
+    find ${SCRIPT_DIR}/../ -type f -name "Dockerfile" -exec sed -i "/${SEARCH_STRING_GIT}/d" {} \;
+
+    for distro in ${DISTROS[@]}; do
+        if [[ ${distro} != "scripts" ]]; then
+            local target_file=${SCRIPT_DIR}/../${distro}/docker-compose.yml
+            local count=$(grep -c ${SEARCH_STRINGS_SSH[0]} ${target_file})
+            if [[ ${count} -ne 0 ]]; then
+                local target_line=$(grep -n "${SEARCH_STRINGS_SSH[0]}" ${target_file} | cut -d ":" -f 1 | head -n 1)
+                sed -i "$((target_line - 2)),$((target_line))d" ${target_file}
+            fi
+        fi
+    done
+
+    echo ""
+    echo "Disabled git sync"
+}
+
+function main() {
+    if [[ $1 == "disable" ]]; then
+        disable_git_sync
+    else
+        enable_git_sync
+    fi
+}
+
+main $@
