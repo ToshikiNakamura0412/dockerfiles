dc5f6862c2e25172de7896c527fee11fc8dfafef
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-09-08 15:02:01 +0900

    Support mac setup

--- scripts/common.bash
+++ scripts/common.bash
@@ -7,6 +7,17 @@ INVALID_DISTROS=(
 ERROR_COUNT_OF_DELETE_LINES=0
 ERROR_COUNT_OF_INSERT_LINES=0
 
+function get_distro() {
+    if [[ "$(uname)" == "Linux" ]] && [[ -e /etc/os-release ]]; then
+        source /etc/os-release
+        echo ${ID}
+    elif [[ "$(uname)" == "Darwin" ]]; then
+        echo "mac"
+    else
+        echo ""
+    fi
+}
+
 function is_invalid_distro() {
     local distro=$1
     if [[ -z ${distro} ]]; then
@@ -34,12 +45,16 @@ function delete_lines() {
         echo -e "\e[31mError: target_strings is empty\e[m"
         return
     fi
-    local search_string=$(echo ${target_strings[-1]} | sed 's/\\//g' | sed 's/^ *//g')
+    local search_string=$(echo ${target_strings[${#target_strings[@]}-1]} | sed 's/\\//g' | sed 's/^ *//g')
 
     if grep -Fq "${search_string}" ${target_file}; then
         while grep -Fq "${search_string}" ${target_file}; do
             target_line=$(grep -Fn "${search_string}" ${target_file} | cut -d ":" -f 1 | head -n 1)
-            sed -i "$((target_line - ${#target_strings[@]} + 1)),$((target_line))d" ${target_file}
+            if [[ $(get_distro) == "mac" ]]; then
+                sed -i '' "$((target_line - ${#target_strings[@]} + 1)),$((target_line))d" ${target_file}
+            else
+                sed -i "$((target_line - ${#target_strings[@]} + 1)),$((target_line))d" ${target_file}
+            fi
         done
     else
         ERROR_COUNT_OF_DELETE_LINES=$((ERROR_COUNT_OF_DELETE_LINES + 1))
@@ -89,7 +104,13 @@ function insert_lines() {
     for ((i=${#target_strings[@]}-1; i>=0; i--)); do
         local target_line=$(grep -Fn "${search_string}" ${target_file} | cut -d ":" -f 1 | head -n 1)
         if [[ -n ${target_line} ]]; then
-            sed -i "${target_line}a ${target_strings[i]}" ${target_file}
+            if [[ $(get_distro) == "mac" ]]; then
+                sed -i '' "${target_line}a\\
+${target_strings[i]}\\
+" ${target_file}
+            else
+                sed -i "${target_line}a ${target_strings[i]}" ${target_file}
+            fi
         else
             echo -e "\e[33mError: '${search_string}' not found in ${target_file}. Failed to insert target strings.\e[m"
             ERROR_COUNT_OF_INSERT_LINES=$((ERROR_COUNT_OF_INSERT_LINES + 1))
