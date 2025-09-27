c834017447acb67be0e6f0fe5ab13ca852cba154
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2025-09-27 21:26:00 +0900

    Support clipboard osc52 for mac (#88)
    * Update Makefile
    
    * Create a script for osc52
    
    * Add blank line
    
    * Support additional invalid distros for osc52 setup
    
    * Add invalid distros for osc52 setup
    
    * Fix variable
    
    * Fix TARGET_STRING

---
+++ scripts/use_osc52.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+source ${SCRIPT_DIR}/common.bash
+
+DISTORS_NOT_COVERED_BY_DOTFILES_SETUP=(
+  "noetic-cuda-opencv"
+  "noetic-cudnn-opencv"
+  "noetic-pcl10"
+  "noetic-pcl14"
+  "noetic-xrdp"
+)
+INVALID_DISTROS+=("${DISTORS_NOT_COVERED_BY_DOTFILES_SETUP[@]}")
+
+TARGET_FILE_NAME="Dockerfile"
+INSERT_POINT_STRING="~/dotfiles/install.bash"
+# TARGET_STRING is inserted once in the line following INSERT_POINT_STRING.
+TARGET_STRING="RUN ~/dotfiles/nvim/scripts/use_osc52.sh"
+
+delete_lines_all_distros ${TARGET_FILE_NAME} "${TARGET_STRING}"
+insert_lines_all_distros ${TARGET_FILE_NAME} ${INSERT_POINT_STRING} "${TARGET_STRING}"
+
+echo ""
+echo "Use OSC52 for clipboard."
+echo "You can copy text from the container to the host clipboard using OSC52."
+echo ""
