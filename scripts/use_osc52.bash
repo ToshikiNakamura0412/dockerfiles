ae818db1f74b7524cdd81d66f7a66806886ca4c5
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2025-09-27 21:04:54 +0900

    Fix TARGET_STRING

--- scripts/use_osc52.bash
+++ scripts/use_osc52.bash
@@ -15,7 +15,7 @@ INVALID_DISTROS+=("${DISTORS_NOT_COVERED_BY_DOTFILES_SETUP[@]}")
 TARGET_FILE_NAME="Dockerfile"
 INSERT_POINT_STRING="~/dotfiles/install.bash"
 # TARGET_STRING is inserted once in the line following INSERT_POINT_STRING.
-TARGET_STRING="\~/dotfiles/nvim/scripts/use_osc52.sh"
+TARGET_STRING="RUN ~/dotfiles/nvim/scripts/use_osc52.sh"
 
 delete_lines_all_distros ${TARGET_FILE_NAME} "${TARGET_STRING}"
 insert_lines_all_distros ${TARGET_FILE_NAME} ${INSERT_POINT_STRING} "${TARGET_STRING}"
